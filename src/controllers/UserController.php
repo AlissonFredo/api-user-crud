0f4dd1872d4ad241713ce7ce41d7f42db2478c79
douglas alisson <douglas.alisson@ibridge.com.br>
2024-08-26 22:23:44 -0300

    refactor: ajustar UserController para utilizar UserService nas operações de usuários

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -2,30 +2,57 @@
 
 namespace app\controllers;
 
+use app\services\UserService;
+
 class UserController
 {
+    private $service;
+
+    public function __construct()
+    {
+        $this->service = new UserService();
+    }
+
     public function show($params)
     {
-        return json_encode(["message" => "chegou em show"]);
+        return json_encode($this->service->show($params));
     }
 
     public function list()
     {
-        return json_encode(["message" => "chegou em list"]);
+        return json_encode($this->service->list());
     }
 
     public function create($request)
     {
-        return json_encode(["message" => "chegou em create"]);
+        $result = $this->service->create($request);
+
+        if ($result) {
+            return json_encode(["message" => "Usuario criado com sucesso"]);
+        } else {
+            return json_encode(["message" => "Falha ao criar o usuario"]);
+        }
     }
 
     public function update($request, $params)
     {
-        return json_encode(["message" => "chegou em update"]);
+        $result = $this->service->update($request, $params);
+
+        if ($result) {
+            return json_encode(["message" => "Usuario atualizado com sucesso"]);
+        } else {
+            return json_encode(["message" => "Falha ao atualizar o usuario"]);
+        }
     }
 
     public function destroy($params)
     {
-        return json_encode(["message" => "chegou em destroy"]);
+        $result = $this->service->destroy($params);
+
+        if ($result) {
+            return json_encode(["message" => "Usuario deletado com sucesso"]);
+        } else {
+            return json_encode(["message" => "Falha ao deletar o usuario"]);
+        }
     }
 }
